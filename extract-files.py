4ee16485e10a1c0305402b697cc8b3c332af3361
Aeron-Aeron <aeronrules2@gmail.com>
2026-01-07 04:35:18 +0000

    rubyx: Bump `hardware.graphics.common` to V7
    Signed-off-by: Aeron-Aeron <aeronrules2@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -36,7 +36,7 @@ blob_fixups: blob_fixups_user_type = {
             .add_needed('libgui_shim_miuicamera.so'),
             
     'system_ext/lib64/vendor.mediatek.hardware.camera.isphal-V1-ndk.so': blob_fixup()
-        .replace_needed('android.hardware.graphics.common-V5-ndk.so','android.hardware.graphics.common-V6-ndk.so'),
+        .replace_needed('android.hardware.graphics.common-V5-ndk.so','android.hardware.graphics.common-V7-ndk.so'),
         
     'system/priv-app/MiuiCamera/MiuiCamera.apk': blob_fixup()
         .apktool_patch('patches/MIUICamera.patch'),
